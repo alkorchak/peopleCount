1fa26a2679a518a69f385db6b967946da6678fae
alkorchak <alkorchak@yandex.ru>
2022-03-13 21:50:23 +0300

    initial commit

---
+++ src/main/kotlin/main.kt
@@ -0,0 +1,23 @@
+fun main() {
+    val likes = 1_066_336_111
+    val digit = likes.toString()
+    val digitLength = digit.length - 1
+    val lastChar = digit[digitLength]
+    val lastLastChar = if (likes > 10 ) digit[digitLength - 1] else digitLength
+    val one = "человеку"
+    val other = "людям"
+
+    if (likes == 1) {
+        println("$likes $one")
+    } else if (likes <= 11) {
+        println("$likes $other")
+    } else if (lastChar.equals(lastLastChar)) {
+        println("$likes $other")
+    } else if (lastChar.equals('1')) {
+        println("$likes $one")
+    } else {
+        println("$likes $other")
+    }
+
+
+}
